d1fc8d17b2ce63869c1af97037c6d8545a42bb1b
BURAK KURT <121500513+burakkurt07@users.noreply.github.com>
2023-06-15 01:51:48 +0300

    Captcha Doğrulama Sistemi kaynak kodları
    Bu kodları makale olarak oku:
    https://medium.com/@burakkurt.me/test-42701bf54234
    
    Bizden bir güvenlik katmanı oluşturmamız ve BOT girişleri engellememiz için bir 4 haneli rastgele sayı doğrulama sistemi tasarlamamızı istendi. Bu sistem, kullanıcıya rastgele 4 haneli bir sayı göstererek, kullanıcının bu sayıyı doğru bir şekilde girmesini isteyecek, kullanıcının üç hakkı olacak. Doğru cevap veren kullanıcılar insan olarak kabul edilecek, yanlış cevaplar ise robotları tespit etmek için kullanılacaktır. Ayrıca, kullanıcılara 3 hakkı vererek doğru cevap vermelerini sağlamamız istendi. Eğer 3 denemede de yanlış cevap verilirse, kullanıcıların erişimi engellenecek.

---
+++ Captcha Doğrulama Sistemi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int remainingAttempts = 3;
+        int generatedNumber = Caphta();
+
+        while (remainingAttempts > 0)
+        {
+            if (CevapAl(generatedNumber))
+            {
+                Console.WriteLine("Siz robot değilsiniz!");
+                return;
+            }
+
+            remainingAttempts--;
+            Console.WriteLine("Yanlış cevap! Kalan hakkınız: " + remainingAttempts);
+        }
+
+        Console.WriteLine("Siz robotsunuz!");
+    }
+
+    static int Caphta()
+    {
+        Random random = new Random();
+        int number = random.Next(1000, 10000);
+        Console.WriteLine("Lütfen aşağıdaki sayıyı doğrulayın: " + number);
+        return number;
+    }
+
+    static bool CevapAl(int generatedNumber)
+    {
+        Console.Write("Sayıyı giriniz: ");
+        int input = Convert.ToInt32(Console.ReadLine());
+        return input == generatedNumber;
+    }
+}
